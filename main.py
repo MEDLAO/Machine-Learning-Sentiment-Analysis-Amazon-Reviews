0b6358759dfd76ed9b23a2f7f873fa13ac7a8a69
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-05-27 19:09:20 +0100

    Compare Vader and RoBERTa results

--- main.py
+++ main.py
@@ -122,15 +122,59 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
 # Running RoBERTa Model :
-encoded_text = tokenizer(example, return_tensors='pt')
+# encoded_text = tokenizer(example, return_tensors='pt')
 # print(encoded_text)
-output = model(**encoded_text)
+# output = model(**encoded_text)
 # print(output)
 # converting the raw model output from a PyTorch tensor into a NumPy array :
-scores = output[0][0].detach().numpy()
+# scores = output[0][0].detach().numpy()
 # print(scores)
-scores = softmax(scores)  # converts a vector of raw scores (logits) into probabilities (0, 1)
+# scores = softmax(scores)  # converts a vector of raw scores (logits) into probabilities (0, 1)
 # print(scores)
-scores_dict = {'roberta-neg': scores[0], 'roberta_neu': scores[1], 'roberta_pos': scores[2]}
+# scores_dict = {'roberta-neg': scores[0], 'roberta_neu': scores[1], 'roberta_pos': scores[2]}
 # print(scores_dict)
 # {'roberta-neg': 0.97635514, 'roberta_neu': 0.020687465, 'roberta_pos': 0.0029573692}
+
+
+def polarity_scores_roberta(examp):
+    encoded_text = tokenizer(examp, return_tensors='pt')
+    output = model(**encoded_text)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    scores_dict = {'roberta_neg': scores[0], 'roberta_neu': scores[1], 'roberta_pos': scores[2]}
+    return scores_dict
+
+
+res = {}
+for i, row in tqdm(df.iterrows(), total=len(df)):  # calculating and storing the polarity score for each row in a dictionary
+    try:
+        text = row['Text']
+        my_id = row['Id']
+        vader_result = sia.polarity_scores(text)
+        vader_result_name = {}
+        for key, value in vader_result.items():
+            vader_result_name[f'vader_{key}'] = value
+        roberta_result = polarity_scores_roberta(text)
+        both = vader_result_name | roberta_result  # merge both dictionaries
+        res[my_id] = both
+    except RuntimeError:
+        print(f'broke for id {my_id}')
+
+# Converting the res dictionary to a dataframe. This time res contains VADER and RoBERTa results
+results_df = pd.DataFrame(res).T
+print(results_df)
+results_df = results_df.reset_index().rename(columns={'index': 'Id'})
+results_df = results_df.merge(df, how='left')
+print(results_df.head())
+
+# 6 - Compare scores between models
+# print(results_df.columns)
+# visualizing the relationship between sentiment scores predicted by VADER and the RoBERTa model,
+# with respect to the review scores :
+sns.pairplot(data=results_df, vars=['vader_neg', 'vader_neu', 'vader_pos', 'roberta_neg',
+                                    'roberta_neu', 'roberta_pos'], hue='Score', palette='tab10')
+# plt.show()
+
+# 7 - Review Examples
+
+
